41b78798b6eadeca2e5728c323f1592b05492151
Yuki Suga <ysuga@ysuga.net>
2014-07-17 22:14:55 +0900

    Update configuration

--- src/ARDroneImpl.java
+++ src/ARDroneImpl.java
@@ -18,16 +18,17 @@ import jp.go.aist.rtm.RTC.util.DataRef;
 import jp.go.aist.rtm.RTC.util.DoubleHolder;
 import jp.go.aist.rtm.RTC.util.StringHolder;
 
-import org.omg.PortableServer.Servant;
 import org.omg.PortableServer.POAPackage.ObjectNotActive;
 import org.omg.PortableServer.POAPackage.ServantAlreadyActive;
 import org.omg.PortableServer.POAPackage.WrongPolicy;
 
 import RTC.CameraImage;
+import RTC.Orientation3D;
 import RTC.ReturnCode_t;
 import RTC.TimedDouble;
 import RTC.TimedOrientation3D;
 import RTC.TimedVelocity3D;
+import RTC.Velocity3D;
 
 import com.shigeodayo.ardrone.navdata.AttitudeListener;
 import com.shigeodayo.ardrone.navdata.VelocityListener;
@@ -50,22 +51,25 @@ public class ARDroneImpl extends DataFlowComponentBase {
 	public ARDroneImpl(Manager manager) {
 		super(manager);
 		// <rtc-template block="initializer">
-        m_targetVelocity_val = new TimedVelocity3D();
-        m_targetVelocity = new DataRef<TimedVelocity3D>(m_targetVelocity_val);
-        m_targetVelocityIn = new InPort<TimedVelocity3D>("targetVelocity", m_targetVelocity);
-        m_camera_val = new CameraImage();
-        m_camera = new DataRef<CameraImage>(m_camera_val);
-        m_cameraOut = new OutPort<CameraImage>("camera", m_camera);
-        m_orientation_val = new TimedOrientation3D();
-        m_orientation = new DataRef<TimedOrientation3D>(m_orientation_val);
-        m_orientationOut = new OutPort<TimedOrientation3D>("orientation", m_orientation);
-        m_attitude_val = new TimedDouble();
-        m_attitude = new DataRef<TimedDouble>(m_attitude_val);
-        m_attitudeOut = new OutPort<TimedDouble>("attitude", m_attitude);
-        m_currentVelocity_val = new TimedVelocity3D();
-        m_currentVelocity = new DataRef<TimedVelocity3D>(m_currentVelocity_val);
-        m_currentVelocityOut = new OutPort<TimedVelocity3D>("currentVelocity", m_currentVelocity);
-        m_DroneServicePort = new CorbaPort("DroneService");
+		m_targetVelocity_val = new TimedVelocity3D();
+		m_targetVelocity = new DataRef<TimedVelocity3D>(m_targetVelocity_val);
+		m_targetVelocityIn = new InPort<TimedVelocity3D>("targetVelocity",
+				m_targetVelocity);
+		m_camera_val = new CameraImage(new RTC.Time(0, 0), (short)0, (short)0, (short)0, "", 0., null);
+		m_camera = new DataRef<CameraImage>(m_camera_val);
+		m_cameraOut = new OutPort<CameraImage>("camera", m_camera);
+		m_orientation_val = new TimedOrientation3D(new RTC.Time(0, 0), new Orientation3D(0., 0., 0.));
+		m_orientation = new DataRef<TimedOrientation3D>(m_orientation_val);
+		m_orientationOut = new OutPort<TimedOrientation3D>("orientation",
+				m_orientation);
+		m_attitude_val = new TimedDouble(new RTC.Time(0, 0), 0.);
+		m_attitude = new DataRef<TimedDouble>(m_attitude_val);
+		m_attitudeOut = new OutPort<TimedDouble>("attitude", m_attitude);
+		m_currentVelocity_val = new TimedVelocity3D(new RTC.Time(0, 0), new Velocity3D(0., 0., 0., 0., 0., 0.));
+		m_currentVelocity = new DataRef<TimedVelocity3D>(m_currentVelocity_val);
+		m_currentVelocityOut = new OutPort<TimedVelocity3D>("currentVelocity",
+				m_currentVelocity);
+		m_DroneServicePort = new CorbaPort("DroneService");
 		// </rtc-template>
 
 	}
@@ -82,36 +86,38 @@ public class ARDroneImpl extends DataFlowComponentBase {
 	protected ReturnCode_t onInitialize() {
 		// Registration: InPort/OutPort/Service
 		// <rtc-template block="registration">
-        // Set InPort buffers
-        addInPort("targetVelocity", m_targetVelocityIn);
-        
-        // Set OutPort buffer
-        addOutPort("camera", m_cameraOut);
-        addOutPort("orientation", m_orientationOut);
-        addOutPort("attitude", m_attitudeOut);
-        addOutPort("currentVelocity", m_currentVelocityOut);
-        
-        // Set service provider to Ports
-        try {
-        	m_DroneServicePort.registerProvider("DroneService", "ssr.DroneService", m_droneService);
-        	m_droneService.setDrone(drone);
-        } catch (ServantAlreadyActive e) {
-            e.printStackTrace();
-        } catch (WrongPolicy e) {
-            e.printStackTrace();
-        } catch (ObjectNotActive e) {
-            e.printStackTrace();
-        }
-        
-        // Set service consumers to Ports
-        
-        // Set CORBA Service Ports
-        addPort(m_DroneServicePort);
+		// Set InPort buffers
+		addInPort("targetVelocity", m_targetVelocityIn);
+
+		// Set OutPort buffer
+		addOutPort("camera", m_cameraOut);
+		addOutPort("orientation", m_orientationOut);
+		addOutPort("attitude", m_attitudeOut);
+		addOutPort("currentVelocity", m_currentVelocityOut);
+
+		// Set service provider to Ports
+		try {
+			m_DroneServicePort.registerProvider("DroneService",
+					"ssr.DroneService", m_droneService);
+			m_droneService.setDrone(drone);
+		} catch (ServantAlreadyActive e) {
+			e.printStackTrace();
+		} catch (WrongPolicy e) {
+			e.printStackTrace();
+		} catch (ObjectNotActive e) {
+			e.printStackTrace();
+		}
+
+		// Set service consumers to Ports
+
+		// Set CORBA Service Ports
+		addPort(m_DroneServicePort);
 		// </rtc-template>
 		bindParameter("velocityGainTranslation", m_velocityGainTranslation,
 				"1.0");
 		bindParameter("velocityGainRotation", m_velocityGainRotation, "1.0");
 		bindParameter("use_camera", m_use_camera, "vertical");
+		bindParameter("ipAddress", m_ipAddress, "192.168.1.1");
 		return super.onInitialize();
 	}
 
@@ -163,6 +169,7 @@ public class ARDroneImpl extends DataFlowComponentBase {
 	// return super.onShutdown(ec_id);
 	// }
 
+	BufferedImage imageBuffer;
 	/***
 	 * 
 	 * The activated action (Active state entry action) former
@@ -179,14 +186,32 @@ public class ARDroneImpl extends DataFlowComponentBase {
 	protected ReturnCode_t onActivated(int ec_id) {
 		drone = new com.shigeodayo.ardrone.ARDrone(this.m_ipAddress.value);
 		drone.connect();
+		
+		m_droneService.setDrone(drone);
 
 		drone.addImageUpdateListener(new ImageListener() {
-
 			@Override
-			public void imageUpdated(BufferedImage arg0) {
-				setImage(arg0);
+			public void imageUpdated(BufferedImage img) {
+				imageBuffer = img;
+				if (imageBuffer != null) {
+					m_camera.v.width = (short) imageBuffer.getWidth();
+					m_camera.v.height = (short) imageBuffer.getHeight();
+					m_camera.v.pixels = new byte[m_camera.v.width * m_camera.v.height * 3];
+					for (int h = 0;h < m_camera.v.height;h++) {
+						for (int w = 0;w < m_camera.v.width;w++) {
+							int index = h * m_camera.v.width + w;
+							int rgb = imageBuffer.getRGB(w,  h);
+							byte r = (byte)(0xFF & (rgb >> 16));
+							byte g = (byte)(0xFF & (rgb >> 8));
+							byte b = (byte)(0xFF & (rgb >> 0));
+							m_camera.v.pixels[index*3 + 2] = r;
+							m_camera.v.pixels[index*3 + 1] = g;
+							m_camera.v.pixels[index*3 + 0] = b;
+						}
+					}
+					m_cameraOut.write();
+				}
 			}
-
 		});
 
 		drone.addAttitudeUpdateListener(new AttitudeListener() {
@@ -194,14 +219,17 @@ public class ARDroneImpl extends DataFlowComponentBase {
 			@Override
 			public void attitudeUpdated(float pitch, float roll, float yaw,
 					int attitude) {
+				
 				m_orientation.v.data.p = pitch;
 				m_orientation.v.data.y = yaw;
 				m_orientation.v.data.r = roll;
 				m_orientationOut.write();
-				m_orientationOut.setTimestamp(m_orientation_val);
+				//m_orientationOut.setTimestamp(m_orientation_val);
 				m_attitude.v.data = attitude;
-				m_attitudeOut.setTimestamp(m_attitude_val);
+				//m_attitudeOut.setTimestamp(m_attitude_val);
 				m_attitudeOut.write();
+			
+			 
 			}
 
 		});
@@ -210,19 +238,21 @@ public class ARDroneImpl extends DataFlowComponentBase {
 
 			@Override
 			public void velocityChanged(float vx, float vy, float vz) {
+				
 				m_currentVelocity.v.data.vx = vx;
 				m_currentVelocity.v.data.vy = vy;
 				m_currentVelocity.v.data.vz = vz;
-				m_currentVelocityOut.setTimestamp(m_currentVelocity_val);
-				m_currentVelocityOut.write();
+				
+			//	m_currentVelocityOut.setTimestamp(m_currentVelocity_val);
+			//	m_currentVelocityOut.write();
 			}
 		});
-		
+
 		drone.connectNav();
 		drone.connectVideo();
 		drone.start();
 
-		drone.reset();
+		//drone.reset();
 		return super.onActivated(ec_id);
 	}
 
@@ -240,7 +270,14 @@ public class ARDroneImpl extends DataFlowComponentBase {
 	 */
 	@Override
 	protected ReturnCode_t onDeactivated(int ec_id) {
+
+		
+		drone.removeAttitudeUpdateListener();
+		drone.removeImageUpdateListener();
+		drone.removeVelocityUpdateListener();
+		
 		drone.stop();
+		drone.reset();
 		drone.disconnect();
 		return super.onDeactivated(ec_id);
 	}
@@ -260,8 +297,13 @@ public class ARDroneImpl extends DataFlowComponentBase {
 	protected ReturnCode_t onExecute(int ec_id) {
 		if (m_targetVelocityIn.isNew()) {
 			m_targetVelocityIn.read();
-			
+			drone.move3D(
+					(int) (this.m_velocityGainTranslation.value * m_targetVelocity.v.data.vx),
+					(int) (this.m_velocityGainTranslation.value * m_targetVelocity.v.data.vy),
+					(int) (this.m_velocityGainTranslation.value * m_targetVelocity.v.data.vz),
+					(int) (this.m_velocityGainRotation.value * m_targetVelocity.v.data.va));
 		}
+		
 		return super.onExecute(ec_id);
 	}
 
@@ -351,96 +393,104 @@ public class ARDroneImpl extends DataFlowComponentBase {
 	//
 	// Configuration variable declaration
 	// <rtc-template block="config_declare">
-    /*!
-     * 
-     * - Name:  velocityGainTranslation
-     * - DefaultValue: 1.0
-     */
-    protected DoubleHolder m_velocityGainTranslation = new DoubleHolder();
-    /*!
-     * 
-     * - Name:  velocityGainRotation
-     * - DefaultValue: 1.0
-     */
-    protected DoubleHolder m_velocityGainRotation = new DoubleHolder();
-    /*!
-     * 
-     * - Name:  use_camera
-     * - DefaultValue: vertical
-     */
-    protected StringHolder m_use_camera = new StringHolder();
-    /*!
-     * 
-     * - Name:  ipAddress
-     * - DefaultValue: 192.168.1.1
-     */
-    protected StringHolder m_ipAddress = new StringHolder();
+	/*
+	 * !
+	 * 
+	 * - Name: velocityGainTranslation - DefaultValue: 1.0
+	 */
+	protected DoubleHolder m_velocityGainTranslation = new DoubleHolder();
+	/*
+	 * !
+	 * 
+	 * - Name: velocityGainRotation - DefaultValue: 1.0
+	 */
+	protected DoubleHolder m_velocityGainRotation = new DoubleHolder();
+	/*
+	 * !
+	 * 
+	 * - Name: use_camera - DefaultValue: vertical
+	 */
+	protected StringHolder m_use_camera = new StringHolder();
+	/*
+	 * !
+	 * 
+	 * - Name: ipAddress - DefaultValue: 192.168.1.1
+	 */
+	protected StringHolder m_ipAddress = new StringHolder();
 	// </rtc-template>
 
 	// DataInPort declaration
 	// <rtc-template block="inport_declare">
-    protected TimedVelocity3D m_targetVelocity_val;
-    protected DataRef<TimedVelocity3D> m_targetVelocity;
-    /*!
-     */
-    protected InPort<TimedVelocity3D> m_targetVelocityIn;
+	protected TimedVelocity3D m_targetVelocity_val;
+	protected DataRef<TimedVelocity3D> m_targetVelocity;
+	/*
+	 * !
+	 */
+	protected InPort<TimedVelocity3D> m_targetVelocityIn;
 
-    
 	// </rtc-template>
 
 	// DataOutPort declaration
 	// <rtc-template block="outport_declare">
-    protected CameraImage m_camera_val;
-    protected DataRef<CameraImage> m_camera;
-    /*!
-     */
-    protected OutPort<CameraImage> m_cameraOut;
+	protected CameraImage m_camera_val;
+	protected DataRef<CameraImage> m_camera;
+	/*
+	 * !
+	 */
+	protected OutPort<CameraImage> m_cameraOut;
 
-    protected TimedOrientation3D m_orientation_val;
-    protected DataRef<TimedOrientation3D> m_orientation;
-    /*!
-     */
-    protected OutPort<TimedOrientation3D> m_orientationOut;
+	protected TimedOrientation3D m_orientation_val;
+	protected DataRef<TimedOrientation3D> m_orientation;
+	/*
+	 * !
+	 */
+	protected OutPort<TimedOrientation3D> m_orientationOut;
 
-    protected TimedDouble m_attitude_val;
-    protected DataRef<TimedDouble> m_attitude;
-    /*!
-     */
-    protected OutPort<TimedDouble> m_attitudeOut;
+	protected TimedDouble m_attitude_val;
+	protected DataRef<TimedDouble> m_attitude;
+	/*
+	 * !
+	 */
+	protected OutPort<TimedDouble> m_attitudeOut;
 
-    protected TimedVelocity3D m_currentVelocity_val;
-    protected DataRef<TimedVelocity3D> m_currentVelocity;
-    /*!
-     */
-    protected OutPort<TimedVelocity3D> m_currentVelocityOut;
+	protected TimedVelocity3D m_currentVelocity_val;
+	protected DataRef<TimedVelocity3D> m_currentVelocity;
+	/*
+	 * !
+	 */
+	protected OutPort<TimedVelocity3D> m_currentVelocityOut;
 
-    
 	// </rtc-template>
 
 	// CORBA Port declaration
 	// <rtc-template block="corbaport_declare">
-    /*!
-     */
-    protected CorbaPort m_DroneServicePort;
-    
+	/*
+	 * !
+	 */
+	protected CorbaPort m_DroneServicePort;
+
 	// </rtc-template>
 
 	// Service declaration
 	// <rtc-template block="service_declare">
-    /*!
-     */
-    protected DroneServiceSVC_impl m_droneService = new DroneServiceSVC_impl();
-    
+	/*
+	 * !
+	 */
+	protected DroneServiceSVC_impl m_droneService = new DroneServiceSVC_impl();
+
 	// </rtc-template>
 
 	// Consumer declaration
 	// <rtc-template block="consumer_declare">
-    
+
 	// </rtc-template>
 
 	com.shigeodayo.ardrone.ARDrone drone;
-	
+
 	BufferedImage image;
-	public void setImage(BufferedImage im) {image = im;}
+
+	public void setImage(BufferedImage im) {
+		image = im;
+	}
 
 }
