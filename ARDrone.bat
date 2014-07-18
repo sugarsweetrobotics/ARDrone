05b121c62fdd29283509cbc62fe7623454fb62d2
ogata-lab <tech@ogata-lab.jp>
2014-07-18 11:38:28 +0900

    Modify Windows Bat file and Build dependent jar file for Win32

--- ARDrone.bat
+++ ARDrone.bat
@@ -1,5 +1,8 @@
-set CLASSPATH=.;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.1.0.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar
-cd /d %~dp0
-cd bin
-java ARDroneComp -f rtc.conf %*
+
+@rem ARDrone RTC実行用スクリプト
+
+@set CLASSPATH=bin;jar\rtcd.jar;jar\rtcprof.jar;jar\OpenRTM-aist-1.1.0.jar;jar\commons-cli-1.1.jar;jar\ARDroneForP5.jar;jar\commons-logging-1.2.jar;jar\commons-net-1.4.1.jar;jar\slf4j-api-1.7.7.jar;jar\xuggle-xuggler-5.4.jar;jar\xuggle-xuggler-arch-i686-w64-mingw32.jar;jar\xuggle-xuggler-noarch-5.4.jar
+
+java ARDroneComp -f rtc.conf
+
 pause;
